1981ce686d4a2b72cd968a940d4c5457e7551093
David Molineus <support@marmalade.de>
2012-11-27 22:04:45 +0100

    add hook setup_extension_hook

--- system/modules/font-awesome/config/config.php
+++ system/modules/font-awesome/config/config.php
@@ -14,6 +14,7 @@
  
 // register hook
 $GLOBALS['TL_HOOKS']['parseTemplate'][] = array('Netzmacht\FontAwesome', 'onParseTemplate');
+$GLOBALS['SETUP_EXT_HOOK']['font-awesome'][] = array('Netzmacht\FontAwesome', 'onParseTemplate');
 
 // load icons config
 if(TL_MODE == 'BE') 
