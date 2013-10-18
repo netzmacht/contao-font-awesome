80891c16f85030ab24200d8695757240630149fa
David Molineus <mail@netzmacht.de>
2013-10-18 10:44:17 +0200

    add english translation for tl_layout

---
+++ src/system/modules/font-awesome/languages/en/tl_layout.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_layout']['fontAwesome'][0] = 'Use FontAwesome';
+$GLOBALS['TL_LANG']['tl_layout']['fontAwesome'][1] = 'Please choose a css file if you want to use FontAwesome icons.';
