2617cefef9c6e29e6a6c7d5b0157677f1eb7a7d3
David Molineus <mail@netzmacht.de>
2014-08-08 09:07:40 +0200

    add compatibility for both bootstrap versions

--- src/Netzmacht/FontAwesome/EventSubscriber.php
+++ src/Netzmacht/FontAwesome/EventSubscriber.php
@@ -12,7 +12,7 @@
 namespace Netzmacht\FontAwesome;
 
 
-use Netzmacht\FormHelper\Event\Events;
+use Netzmacht\Bootstrap\Form\InputGroup;
 use Netzmacht\FormHelper\Event\GenerateEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -26,41 +26,91 @@ class EventSubscriber
 	{
 		$widget 	= $event->getWidget();
 		$inputGroup = $event->getContainer()->getWrapper();
+		$template   = static::getIconSetTemplateWhenEnabled($inputGroup, $widget);
 
-		// no input group or no bootstrap extension available or not enabled
-		if(!$inputGroup || ! class_exists('Netzmacht\Bootstrap\Bootstrap') || !\Netzmacht\Bootstrap\Bootstrap::isEnabled()) {
+		if(!$template) {
 			return;
 		}
 
-		// wrapper is no input group, do nothing
-		if(!$inputGroup instanceof \Netzmacht\Bootstrap\Form\InputGroup) {
-			return;
+		$icon = sprintf($template, $widget->bootstrap_icon . ' fa-fw');
+
+		if($widget->bootstrap_iconPosition == 'right') {
+			$inputGroup->setRight($icon);
 		}
-
-		// other icon set is loaded
-		if($GLOBALS['BOOTSTRAP']['icons']['active'] != 'font-awesome') {
-			return;
-		}
-
-		// fa-fw is applied in the icon set templates
-		if(strpos($GLOBALS['BOOTSTRAP']['icons']['sets']['font-awesome']['template'], 'fa-fw') !== false) {
-			return;
-		}
-
-		// add icon
-		if($widget->bootstrap_addIcon) {
-			$icon = sprintf(
-				$GLOBALS['BOOTSTRAP']['icons']['sets']['font-awesome']['template'],
-				$widget->bootstrap_icon . ' fa-fw'
-			);
-
-			if($widget->bootstrap_iconPosition == 'right') {
-				$inputGroup->setRight($icon);
-			}
-			else {
-				$inputGroup->setLeft($icon);
-			}
+		else {
+			$inputGroup->setLeft($icon);
 		}
 	}
 
-} 
+
+	/**
+	 * @param $inputGroup
+	 * @param $widget
+	 * @return bool|string
+	 */
+	private static function getIconSetTemplateWhenEnabled($inputGroup, $widget)
+	{
+		// wrapper is no input group, do nothing
+		if(!$inputGroup || !$inputGroup instanceof \Netzmacht\Bootstrap\Form\InputGroup) {
+			return false;
+		}
+
+		if(static::isLegacyBootstrapEnabled()) {
+			$name     = $GLOBALS['BOOTSTRAP']['icons']['active'];
+			$template = $GLOBALS['BOOTSTRAP']['icons']['sets']['font-awesome']['template'];
+		}
+		elseif(static::isBootstrapEnabled()) {
+			$iconSet  = \Netzmacht\Bootstrap\Core\Bootstrap::getIconSet();
+			$name     = $iconSet->getIconSetName();
+			$template = $iconSet->getTemplate();
+		}
+		else {
+			return false;
+		}
+
+		// other icon set is loaded
+		if($name != 'font-awesome') {
+			return false;
+		}
+
+		// fa-fw is applied in the icon set templates
+		if(strpos($template, 'fa-fw') !== false) {
+			return false;
+		}
+
+		if(!$widget->bootstrap_addIcon) {
+			return false;
+		}
+
+		return $template;
+	}
+
+
+	/**
+	 *
+	 */
+	private static function isLegacyBootstrapEnabled()
+	{
+		// no input group or no bootstrap extension available or not enabled
+		if(class_exists('Netzmacht\Bootstrap\Bootstrap') && \Netzmacht\Bootstrap\Bootstrap::isEnabled()) {
+			return true;
+
+		}
+
+		return false;
+	}
+
+
+	/**
+	 *
+	 */
+	private static function isBootstrapEnabled()
+	{
+		if(class_exists('Netzmacht\Bootstrap\Core\Bootstrap') && \Netzmacht\Bootstrap\Core\Bootstrap::isEnabled()) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
